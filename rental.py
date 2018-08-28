5f0c15dce1e2362c765c862b56d9f67f3449dcd8
jihwan <ralbu85@gmail.com>
2018-08-28 20:16:57 +0900

    second trial

--- rental.py
+++ rental.py
@@ -4,10 +4,157 @@ from tkinter import ttk
 import books as books
 import datetime
 
+class Rental():
+    def __init__(self, master):
+
+        ####### Master  #############
+        self.rent_master=master
+        style = ttk.Style()
+        style.configure("Treeview.Heading", font=(None, 10))
+
+        self.mainFrame=_mainFrame(self.rent_master, row=0, column=0)
+        self.menuFrame =_menuFrame(self.rent_master, row=0, column=1)
+
+        self.message=_message(self.mainFrame.frame, row=1, column=0)
+        self.rentFrame=_rentFrame(self.mainFrame.frame, row=2, column=0)
+        self.searchFrame=_searchFrame(self.mainFrame.frame, self.message, self.rentFrame, row=0, column=0)
+
+class _mainFrame():
+    def __init__(self, parent, *args, **kwargs):
+        self.frame=Frame(parent)
+        print(kwargs['row'])
+        self.frame.grid(row=kwargs['row'], column=kwargs['column'], padx=3)
+
+class _message():
+    def __init__(self, parent, *args, **kwargs):
+        self.frame=Frame(parent)
+        self.frame.grid(row=kwargs['row'], column=kwargs['column'], pady=10, sticky=W, padx=10)
+
+        self.status_message=Label(self.frame, text='', fg='red', width=15)
+        self.status_message.grid(row=0, column=0)
+
+        self.member_info=Frame(self.frame)
+        self.member_info.grid(row=0, column=1)
+
+        Label(self.member_info, text='이름:', fg='blue').grid(row=0, column=0, sticky=W, padx=10 )
+        self.name=Label(self.member_info, text='', fg='blue')
+        self.name.grid(row=0, column=1, sticky=W, padx=10 )
+
+        Label(self.member_info, text='휴대폰:', fg='blue').grid(row=1, column=0, sticky=W, padx=10 )
+        self.phone=Label(self.member_info, text='', fg='blue')
+        self.phone.grid(row=1, column=1, sticky=W, padx=10 )
+
+        Label(self.member_info, text='주소:', fg='blue').grid(row=2, column=0, sticky=W, padx=10 )
+        self.address=Label(self.member_info, text='', fg='blue')
+        self.address.grid(row=2, column=1, sticky=W, padx=10 )
+
+    def update_status_info(self, message):
+        self.status_message['text']=message
+
+    def update_member_info(self, row):
+        self.name['text']=row[1]
+        self.phone['text']=row[2]
+        self.address['text']=row[3]
+
+class _rentFrame():
+    def __init__(self, parent, *args, **kwargs):
+        ############# Tree ###################
+        self.tree = ttk.Treeview(parent, height=15, column=('rent_date', 'title', 'vol', 'genre', 'author', 'days'),
+                                 show='headings', selectmode="browse")
+        self.tree.grid(row=kwargs['row'], column=kwargs['column'], pady=10)
+
+        # scrollbar
+        ysb = ttk.Scrollbar(parent, orient='vertical', command=self.tree.yview)
+        ysb.grid(row=2, column=1, sticky='ns', pady=10)
+        self.tree.configure(yscroll=ysb.set)
+
+        self.tree.heading('#1', text='대여일')
+        self.tree.heading('#2', text='제목')
+        self.tree.heading('#3', text='권')
+        self.tree.heading('#4', text='장르')
+        self.tree.heading('#5', text='저자')
+        self.tree.heading('#6', text='경과일')
+
+        self.tree.column('rent_date', width=150, anchor=CENTER)
+        self.tree.column('title', width=170, anchor=CENTER)
+        self.tree.column('vol', width=40, anchor=CENTER)
+        self.tree.column('genre', width=40, anchor=CENTER)
+        self.tree.column('author', width=40, anchor=CENTER)
+        self.tree.column('days', width=50, anchor=CENTER)
+        #####################################
+
+    def update_rent_info(self, member_id):
+        con = sq.connect('guelbang.db')
+        c = con.cursor()
+        query = 'SELECT RentDate, Title, Vol, Genre, AUthor FROM RENT join BOOK on BOOK.Id=RENT.BookId where MemberId=?'
+        parameters = (member_id,)
+        c.execute(query, parameters)
+
+        rows = c.fetchall()
+        for row in rows:
+            delta = datetime.datetime.now() - datetime.datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S")
+            row=row + (delta.days,)
+            self.tree.insert("", END, values=row)
+
+            c.close()
+
+    def clear_rent_info(self):
+        self.tree.delete(*self.tree.get_children())
+
+class _searchFrame():
+    def __init__(self, parent, message, tree, *args, **kwargs):
+
+        # setting Frame
+        self.searchFrame = Frame(parent)
+        self.searchFrame.grid(row=kwargs['row'], column=kwargs['column'], sticky=W)
+        # search bar
+        self.searchMember = Entry(self.searchFrame, width=20)
+        self.searchMember.grid(row=0, column=0, padx=10, sticky=W)
+        # connected Frame
+        self.message=message
+        self.tree=tree
+        # button
+        Button(self.searchFrame, text='검색', command= lambda:self.search_members(self.searchMember.get())).grid(row=0,column=1)
+
+    def search_validation(self, parameters):
+        return len(parameters)!=0
+
+    def search_members(self, parameters):
+        if self.search_validation(parameters):
+
+            member_row=self.search_query_return(parameters)
+
+            if len(member_row)==0:
+                self.message.update_status_info('검색결과가 없습니다')
+
+            elif len(member_row)==1:
+                self.message.update_member_info(member_row[0])
+                self.tree.clear_rent_info()
+                self.tree.update_rent_info(member_row[0][0])
+            else:
+                self.popup=PopupMember(self.searchFrame, self.message, self.tree, member_row)
+
+        else:
+            self.message.update_status_info('글자를 입력하세요')
+
+    def search_query_return(self, parameters):
+        con = sq.connect('guelbang.db')
+        c = con.cursor()
+        query = 'select Id, Name,Phone,Address from MEMBER where Name like ?'
+        c.execute(query, ('%{}%'.format(parameters),))
+        rows=c.fetchall()
+        c.close()
+        return rows
+
 class PopupMember():
-    def __init__(self, parent, fetch_rows):
+    def __init__(self, parent, message, tree, fetch_rows):
+
         ## popup new window
         self.win_master =Toplevel(parent)
+
+        # connected widget
+        self.message=message
+        self.rentFrame=tree
 
         # determine popup position
         x_axis = parent.winfo_x()
@@ -40,170 +187,24 @@ class PopupMember():
 
     def proceed(self):
         self.selected_row=tuple(self.tree.item(self.tree.selection())['values'])
-        #self.selected=tuple(selected_row)
-        #print (tuple(self.selected_row))
+        self.message.update_member_info(self.selected_row)
+        self.rentFrame.clear_rent_info()
+        self.rentFrame.update_rent_info(self.selected_row[0])
         self.win_master.destroy()
         return self.selected_row
 
+class _menuFrame():
+    def __init__(self, parent, *args, **kwargs):
 
-class MemberStatus():
-    def __init__(self, parent, fetch_rows):
-        print (fetch_rows)
-        self.frame=Frame(parent)
-        self.id=fetch_rows[0]
-        Label(self.frame, text='이름: ').grid(row=0, column=0, sticky=W)
-        Label(self.frame, text=fetch_rows[1]).grid(row=0, column=1, sticky=W)
+        self.menuFrame=Frame(parent)
+        self.menuFrame.grid(row=kwargs['row'],column=kwargs['column'], padx=3, pady=40, sticky=N)
 
-        Label(self.frame, text='핸드폰: ').grid(row=1, column=0, sticky=W)
-        Label(self.frame, text=fetch_rows[2]).grid(row=1, column=1, sticky=W)
-
-        Label(self.frame, text='주소: ').grid(row=2, column=0, sticky=W)
-        Label(self.frame, text=fetch_rows[3]).grid(row=2, column=1, sticky=W)
-
-    def get_frame(self):
-        return self.frame
-
-    def get_id(self):
-        return self.id
-
-class RentStatus():
-    def __init__(self,tree,member_id):
-        ## treeview
-        self.tree=tree
-        self.tree.delete(*tree.get_children())
-        self.printRents(member_id)
-
-    def printRents(self,member_id):
-        con=sq.connect('guelbang.db')
-        c=con.cursor()
-        query='SELECT RentDate, Title, Vol, Genre, AUthor FROM RENT join BOOK on BOOK.Id=RENT.BookId where MemberId=?'
-        parameters = (member_id, )
-        c.execute(query, parameters)
-
-        rows=c.fetchall()
-        for row in rows:
-            delta=datetime.datetime.now()-datetime.datetime.strptime(row[0],"%Y-%m-%d %H:%M:%S")
-            print(row + (delta.days,))
-            self.tree.insert("",END,values=row)
-
-        c.close()
-
-
-class Rental():
-
-    def __init__(self, master):
-
-        #############################
-        ####### Master  #############
-        #############################
-        self.rent_master=master
-        style = ttk.Style()
-        style.configure("Treeview.Heading", font=(None, 10))
-
-        #############################
-        ####### Main Frame ##########
-        #############################
-        self.mainFrame = Frame(self.rent_master)
-        self.mainFrame.grid(row=0, column=0, padx=3)
-
-        # message
-        self.message=Label(self.mainFrame, text='', fg='red')
-        self.message.grid(row=0, column=0, pady=10, sticky=W, padx=10)
-
-
-        ############ Search Frame ###########
-        self.searchFrame = Frame(self.mainFrame)
-        self.searchFrame.grid(row=1, column=0, sticky=W)
-        # search bar
-        self.searchMember = Entry(self.searchFrame, width=20)
-        self.searchMember.grid(row=0, column=0, padx=10, sticky=W)
-        # button
-        Button(self.searchFrame, text='검색', command=lambda: self.search_members(self.searchMember.get())).grid(row=0,column=1)
-        ######################################
-
-        ############# Tree ###################
-        self.tree = ttk.Treeview(self.mainFrame, height=15, column=('rent_date', 'title', 'vol', 'genre', 'author', 'days'),
-                                 show='headings', selectmode="browse")
-        self.tree.grid(row=2, column=0, pady=10)
-
-        # scrollbar
-        ysb = ttk.Scrollbar(self.mainFrame, orient='vertical', command=self.tree.yview)
-        ysb.grid(row=2, column=1, sticky='ns', pady=10)
-        self.tree.configure(yscroll=ysb.set)
-
-        self.tree.heading('#1', text='대여일')
-        self.tree.heading('#2', text='제목')
-        self.tree.heading('#3', text='권')
-        self.tree.heading('#4', text='장르')
-        self.tree.heading('#5', text='저자')
-        self.tree.heading('#6', text='경과일')
-
-        self.tree.column('rent_date', width=150, anchor=CENTER)
-        self.tree.column('title', width=170, anchor=CENTER)
-        self.tree.column('vol', width=40, anchor=CENTER)
-        self.tree.column('genre', width=40, anchor=CENTER)
-        self.tree.column('author', width=40, anchor=CENTER)
-        self.tree.column('days', width=50, anchor=CENTER)
-        #####################################
-
-
-        #############################
-        ####### Menu Frame ##########
-        #############################
-        self.menuFrame=Frame(self.rent_master)
-        self.menuFrame.grid(row=0,column=1, padx=3, pady=40, sticky=N)
-
-        Button(self.menuFrame, text='회원추가',command=self.add_rent, width=10).grid(row=2, padx=3, pady=10)
+        Button(self.menuFrame, text='회원추가',command=self.pop_add_rent, width=10).grid(row=2, padx=3, pady=10)
         Button(self.menuFrame, text='회원삭제',command=self.return_rent, width=10).grid(row=3, padx=3, pady=10)
         Button(self.menuFrame, text='회원정보수정',command=self.view_history, width=10).grid(row=4, padx=3, pady=10)
 
-
-    def search_validation(self, parameters):
-        return len(parameters)!=0
-
-    def search_members(self, parameters):
-        if self.search_validation(parameters):
-            con = sq.connect('guelbang.db')
-            c = con.cursor()
-            query='select * from MEMBER where Name like ?'
-            #parameters=self.searchBook.get()
-            c.execute(query,('%{}%'.format(parameters),))
-            #c.execute(query,parameters)
-
-            rows = c.fetchall()
-            if len(rows)==0:
-                self.message['text']='검색결과가 없습니다'
-
-            elif len(rows)==1:
-                print (rows[0])
-                # frame=MemberStatus(self.searchFrame,rows[0])
-                # frame.get_frame().grid(row=0, column=2, padx=20, sticky=N)
-                # tree=RentStatus(self.tree,frame.get_id())
-
-            else:
-                print('popup')
-                PopupMember(self.mainFrame, rows)
-                print (PopupMember.selected_row)
-
-                # frame=MemberStatus(self.searchFrame, row)
-                # tree = RentStatus(self.tree, frame.get_id())
-
-            c.close()
-
-        else:
-            self.message['text']='글자를 입력하세요'
-
-    def printMembers(self):
-        con = sq.connect('guelbang.db')
-        c = con.cursor()
-        c.execute('select * from MEMBER')
-
-        rows=c.fetchall()
-        for row in rows:
-            self.tree.insert("",END,values=row)
-
-    def add_rent(self):
-        return
+    def pop_add_rent(self):
+        add_rent(self.menuFrame)
 
     def return_rent(self):
         return
@@ -211,8 +212,63 @@ class Rental():
     def view_history(self):
         return
 
-def main():
+class add_rent():
+    def __init__(self,parent,**kwargs):
+        ## popup new window
+        self.win_master = Toplevel(parent)
 
+        # generate Frame within windows
+        self.mainFrame = Frame(self.win_master)
+        self.mainFrame.grid(row=0, column=0,padx=10, pady=5)
+
+        # generate Entry
+        self.entryFrame = Frame(self.mainFrame)
+        self.entryFrame.grid(row=0, column=0)
+
+        Label(self.entryFrame, text='도서검색: ').grid(row=0, column=0)
+        self.entry=Entry(self.entryFrame, width=20)
+        self.entry.grid(row=0,column=1)
+        Button(self.entryFrame, text='검색', command=self.button_pressed).grid(row=0, column=2)
+
+        # generate treeviews
+        self.tree = ttk.Treeview(self.mainFrame, height=20, column=('title','vol','genre','author'),show='headings')
+        self.tree.grid(row=1, column=0)
+
+        self.tree.heading('#1', text='제목')
+        self.tree.heading('#2', text='권')
+        self.tree.heading('#3', text='장르')
+        self.tree.heading('#4', text='저자')
+
+        self.tree.column('title',width=200, anchor=CENTER)
+        self.tree.column('vol',width=40, anchor=CENTER)
+        self.tree.column('genre', width=80, anchor=CENTER)
+        self.tree.column('author', width=80, anchor=CENTER)
+
+    def button_pressed(self):
+        self.keyword=self.entry.get()
+        self.get_query_result()
+        self.update_tree()
+
+    def get_query_result(self):
+        con=sq.connect('guelbang.db')
+        c=con.cursor()
+        query='SELECT Title, Vol, Genre, Author FROM BOOK WHERE Title like ?'
+        c.execute(query, ('%{}%'.format(self.keyword),))
+        self.rows=c.fetchall()
+        c.close()
+
+    def update_tree(self):
+        for row in self.rows:
+            self.tree.insert("",END,values=row)
+
+    def clear_rent_info(self):
+        self.tree.delete(*self.tree.get_children())
+
+
+
+
+
+def main():
     root=Tk()
     myGUIWelcome=Rental(root)
     root.mainloop()
